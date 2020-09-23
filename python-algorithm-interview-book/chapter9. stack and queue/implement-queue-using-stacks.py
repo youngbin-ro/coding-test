90b1e15ef452b603c5053f0a43c822bb1cd1416a
youngbin-ro <youngbin.ro@gmail.com>
2020-09-23 21:42:07 +0900

    add: chapter9 problem24

---
+++ python-algorithm-interview-book/chapter9. stack and queue/implement-queue-using-stacks.py
@@ -0,0 +1,80 @@
+class MyFirstQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        value = self.queue[0]
+        del self.queue[0]
+        return value
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return len(self.queue) == 0
+
+
+class MySecondQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.input = []
+        self.output = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.input.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        self.peek()
+        return self.output.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+        return self.output[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return self.input == [] and self.output == []
+
+
+if __name__ == "__main__":
+    queue = MySecondQueue()
+    queue.push(1)
+    queue.push(2)
+    print(queue.peek())
+    print(queue.pop())
+    print(queue.empty())
