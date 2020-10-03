cde47437df1c3f47f41ad87377cb561fc56027a7
youngbin-ro <youngbin.ro@gmail.com>
2020-10-03 14:37:10 +0900

    add: function for converting tree to list

--- python-algorithm-interview-book/chapter14. tree/tree.py
+++ python-algorithm-interview-book/chapter14. tree/tree.py
@@ -1,3 +1,6 @@
+import collections
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -20,3 +23,21 @@ def make_binary_tree(values: list) -> TreeNode:
             nodes[div].left = node
         nodes.append(node)
     return root
+
+
+def tree2list(root: TreeNode) -> list:
+    if not root:
+        return []
+    queue = collections.deque([root])
+    result = [root.val]
+    while queue:
+        node = queue.popleft()
+        if not node or not (node.left or node.right):
+            continue
+        for child in [node.left, node.right]:
+            if child:
+                result.append(child.val)
+                queue.append(child)
+            else:
+                result.append(None)
+    return result
