b15fb81a305022f32b89845273c054d99f07598f
youngbin-ro <youngbin.ro@gmail.com>
2020-10-12 13:03:09 +0900

    add: trie implementation for palindrome problem

--- python-algorithm-interview-book/chapter16. trie/trie.py
+++ python-algorithm-interview-book/chapter16. trie/trie.py
@@ -1,4 +1,5 @@
 import collections
+from typing import List
 
 
 class TrieNode:
@@ -33,6 +34,72 @@ class Trie:
                 return False
             node = node.children[char]
         return node.word
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+
+class TrieNode4Palindrome:
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode4Palindrome)
+        self.word_id = -1
+        self.palindrome_word_ids = []
+
+
+class Trie4Palindrome:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode4Palindrome()
+
+    @staticmethod
+    def is_palindrome(word: str) -> bool:
+        return word[::] == word[::-1]
+
+    def insert(self, word: str, index: int) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for i, char in enumerate(reversed(word)):
+            if self.is_palindrome(word[:len(word) - i]):
+                node.palindrome_word_ids.append(index)
+            node = node.children[char]
+            node.val = char
+        node.word_id = index
+
+    def search(self, word: str, index: int) -> List[List[int]]:
+        """
+        Returns if the word is in the trie.
+        """
+        results = []
+        node = self.root
+
+        while word:
+            if node.word_id >= 0:
+                if self.is_palindrome(word):
+                    results.append([index, node.word_id])
+            if not word[0] in node.children:
+                return results
+            node = node.children[word[0]]
+            word = word[1:]
+
+        if node.word_id >= 0 and node.word_id != index:
+            results.append([index, node.word_id])
+
+        for palindrome_word_id in node.palindrome_word_ids:
+            results.append([index, palindrome_word_id])
+
+        return results
 
     def startsWith(self, prefix: str) -> bool:
         """
