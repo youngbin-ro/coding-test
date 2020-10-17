6e6ec581165dc9b61bc6e0b20556a9c487c9cc2c
youngbin-ro <youngbin.ro@gmail.com>
2020-10-17 10:38:11 +0900

    add: chapter17 problem62

---
+++ python-algorithm-interview-book/chapter17. sorting/valid-anagram.py
@@ -0,0 +1,10 @@
+def first_solution(s: str, t: str) -> bool:
+    return set(s) == set(t)
+
+
+if __name__ == "__main__":
+    s, t = "anagram", "nagaram"
+    print(first_solution(s, t))
+
+    s, t = "rat", "car"
+    print(first_solution(s, t))
